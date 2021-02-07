579b8c248355103c68f8403a15afe81d6914bd6b
Griffin <gdube@g.clemson.edu>
2021-02-07 00:54:29 -0500

    adding rle compression and helper funcitons

--- lab2.c
+++ lab2.c
@@ -4,41 +4,197 @@
 #include <string.h>
 #include <assert.h>
 
+#define NOT_DEBUG
+#ifdef DEBUG
+#define debug(fmt, ...) fprintf(stderr, fmt, __VA_ARGS__)
+#else
+#define debug(fmt, ...)
+#endif
+
 void parse_input(int argc, char** argv, char* filename, char* compress, char* decompress);
+void read_file(char* filename, unsigned char** data, long int *filesize);
+void write_file(char* filename, unsigned char* data, long int filesize);
+void rle_a(unsigned char* data, unsigned char **archive, long int filesize, long int *archivesize);
+void rle_x(unsigned char** data, unsigned char* archive, long int *filesize, long int archivesize);
+void compress(char* file);
+void decompress(char* file);
 
 int main (int argc, char** argv)
 {
 	char comp, decomp;
 	char file[80];
-	int i;
-	FILE* fp;
+
 	parse_input(argc,argv,file,&comp,&decomp);
-	printf("Compression = %s\n", (comp) ? "true" : "false");
-	printf("Decompression = %s\n", (decomp) ? "true" : "false");
+
+	if (comp == 1) compress(file);
+	else if (decomp == 1) decompress(file);
 
 	return 0;
+}
+
+void rle_a(unsigned char* data, unsigned char **archive, long int filesize, long int *archivesize)
+{
+	int i = 0, j = 0;
+	unsigned char count = 0;
+	unsigned char curr, next;
+
+	*archive = (unsigned char *)malloc(sizeof(unsigned char) * filesize);
+
+	for (i = 0, j = 0; i < filesize; i++, j += 2)
+	{
+		count = 1;
+
+		// set current and next values
+		curr = data[i];
+		next = data[i + 1];
+
+		// compare current value to next for match, check for overflow
+		while (count < 255 && curr == next)
+		{
+			i++;
+			count++;
+
+			curr = data[i];
+			next = data[i + 1];
+		}
+
+		// once string of same values ends, write to archive
+		(*archive)[j] = count;
+		(*archive)[j + 1] = data[i];
+		debug("%d%c",(*archive)[j],(*archive)[j+1]);
+	}
+	*archivesize = j * sizeof(unsigned char);
+	debug("\nArchive Size: %ld\n",*archivesize);
+}
+
+void rle_x(unsigned char** data, unsigned char* archive, long int *filesize, long int archivesize)
+{
+
+}
+
+void decompress(char* file)
+{
+	unsigned char* data, *archive;
+	long int filesize, archivesize;
+	int len;
+	char outfile[80];
+
+	read_file(file, &data, &filesize);
+
+	// compress with RLE
+	printf("Decompressing file... %s\n", file);
+	//rle_x(&data, archive, &filesize, archivesize);
+
+	// append file with *.a file extension
+	strcpy(outfile,file);
+	if (strstr(file, ".a") != NULL)
+	{
+		len = strlen(outfile);
+		strcpy(&outfile[len - 2],&outfile[2 + len - 2]); //get rid of archive extension
+	}
+
+	write_file(outfile, data, filesize);
+
+	return;
+}
+
+void compress(char* file)
+{
+	unsigned char* data, *archive;
+	long int filesize, archivesize;
+	char outfile[80];
+
+	read_file(file, &data, &filesize);
+
+	// compress with RLE
+	printf("Compressing file... %s\n", file);
+	rle_a(data, &archive, filesize, &archivesize);
+
+	// append file with *.a file extension
+	strcpy(outfile,file);
+	strcat(outfile,".a");
+
+	write_file(outfile, archive, archivesize);
+
+	return;
+}
+
+void read_file(char* filename, unsigned char** data, long int *filesize)
+{
+	FILE* fp = NULL;
+
+	fp = fopen(filename,"rb");
+	if (fp == NULL)
+	{
+		printf("Unable to open file %s\n", filename);
+		exit(0);
+	}
+
+	// find filesize and alloc mem
+	fseek(fp, 0, SEEK_END);
+	*filesize = (long int)ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	debug("Reading %ld bytes from %s\n", *filesize, filename);
+
+	// read data
+	*data = (unsigned char *)malloc((*filesize) * sizeof(unsigned char));
+	fread(*data, sizeof(unsigned char), *filesize, fp);
+	fclose(fp);
+
+	return;
+}
+
+void write_file(char* filename, unsigned char* data, long int filesize)
+{
+	FILE* fp = NULL;
+
+	fp = fopen(filename, "wb");
+	if (fp == NULL)
+	{
+		printf("Unable to open file %s\n", filename);
+		exit(0);
+	}
+
+	debug("Writing %ld bytes to %s\n", filesize, filename);
+
+	fwrite(data, sizeof(unsigned char), filesize, fp);
+	fclose(fp);
+
+	return;
 }
 
 void parse_input(int argc, char** argv, char* filename, char* compress, char* decompress)
 {
 	int opt;
+	if (argc <= 2)
+	{
+		printf("Usage: ./rle <-x|-c> filename\n");
+		exit(0);
+	}
+
 	while ((opt = getopt(argc, argv, ":ax")) != -1)
 	{
 		switch(opt)
 		{
 			case 'a':
-				//printf("Performing RLE Compression on %s\n", &optarg);
-				//strcpy(filename, optarg);
 				*compress = 1;
 				break;
 			case 'x':
-				//printf("Performing RLE Decompression on %s\n", &optarg);
-				//strcpy(filename, optarg);
 				*decompress = 1;
 				break;
 		}
 	}
 
-	assert(*compress != 1 || *decompress != 1);
+	assert((*compress != 1 || *decompress != 1) && "Cannot compress and decompress simultaneously... duh\n");
+
+	// determine input/output filename
+	if (argc == 3) strcpy(filename, argv[2]);
+	else
+	{
+		printf("Usage: ./rle <-x|-c> filename\n");
+		exit(0);
+	}
+
 	return;
 }
