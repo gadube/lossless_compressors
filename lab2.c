5bd0beb7b0cf2c6e5835923fa4f946a01e6307ef
Griffin <gdube@g.clemson.edu>
2021-02-07 20:29:48 -0500

    Adding x and a with markers for neg. compression

--- lab2.c
+++ lab2.c
@@ -11,18 +11,26 @@
 #define debug(fmt, ...)
 #endif
 
+#define WINDOW 3
+#define MARKER 0
+
+#define TRUE 1
+#define FALSE 0
+
 void parse_input(int argc, char** argv, char* filename, char* compress, char* decompress);
 void read_file(char* filename, unsigned char** data, long int *filesize);
 void write_file(char* filename, unsigned char* data, long int filesize);
 void rle_a(unsigned char* data, unsigned char **archive, long int filesize, long int *archivesize);
+void rle_markers_a(unsigned char* data, unsigned char **archive, long int filesize, long int *archivesize);
 void rle_x(unsigned char** data, unsigned char* archive, long int *filesize, long int archivesize);
+void rle_markers_x(unsigned char** data, unsigned char* archive, long int *filesize, long int archivesize);
 void print_stats(char* infile, char* outfile, long int filesize, long int archivesize);
 void compress(char* file);
 void decompress(char* file);
 
 int main (int argc, char** argv)
 {
-	char comp, decomp;
+	char comp = 0, decomp = 0;
 	char file[80];
 
 	parse_input(argc,argv,file,&comp,&decomp);
@@ -31,6 +39,150 @@ int main (int argc, char** argv)
 	else if (decomp == 1) decompress(file);
 
 	return 0;
+}
+
+void rle_markers_a(unsigned char* data, unsigned char **archive, long int filesize, long int *archivesize)
+{
+	int i = 0, j = 0, k = 0;
+	int diffrun = 0;
+	unsigned char A_FLAG = 0;
+	unsigned char count = 0;
+	unsigned char curr, next;
+
+	*archive = (unsigned char *)malloc(sizeof(unsigned char) * filesize);
+
+	for (i = 0, j = 0; i < filesize; i++, j += 2)
+	{
+		count = 1;
+
+		// set current and next values
+		curr = data[i];
+		next = data[i + 1];
+
+		k = i;
+		diffrun = 0;
+		while (data[k] != data[k + 1])
+		{
+			k++;
+			diffrun++;
+		}
+
+		// if run of single values is larger than window, turn off compression
+		if (diffrun >= WINDOW) A_FLAG = FALSE;
+		else A_FLAG = TRUE;
+
+		if (A_FLAG)
+		{
+
+			// compare current value to next for match, check for overflow
+			while (count < 255 && curr == next)
+			{
+				i++;
+				count++;
+
+				curr = data[i];
+				next = data[i + 1];
+			}
+
+			// once string of same values ends, write to archive
+			(*archive)[j] = count;
+			(*archive)[j + 1] = data[i];
+			debug("%d%c",(*archive)[j],(*archive)[j+1]);
+		}
+		else
+		{
+			(*archive)[j] = MARKER;
+			(*archive)[j + 1] = 'x';
+			for (k = j + 2; k <= diffrun + j + 1; k++)
+			{
+				if (data[i] == 0)
+				{
+					(*archive)[k] = MARKER;
+					(*archive)[k + 1] = 1;
+					(*archive)[k + 2] = data[i];
+					j += 2;
+					k += 2;
+					i++;
+					A_FLAG = FALSE;
+					break;
+				}
+				else
+				{
+					(*archive)[k] = data[i];
+					i++;
+				}
+			}
+			j = k;
+			A_FLAG = FALSE;
+			(*archive)[j] = MARKER;
+			if ( j % 2 != 0)
+			{
+				(*archive)[j+1] = 'x';
+				j++;
+			}
+			j--;
+			i--;
+		}
+	}
+	*archivesize = j * sizeof(unsigned char);
+	debug("\nArchive Size: %ld\n",*archivesize);
+}
+
+void rle_markers_x(unsigned char** data, unsigned char* archive, long int *filesize, long int archivesize)
+{
+	int i, j;
+	unsigned char count = 0;
+	unsigned char val;
+
+
+	// determine filesize of decompressed values
+	*filesize = 0;
+	for (i = 0; i < archivesize; i += 2)
+	{
+		if (archive[i] != 0) *filesize += archive[i];
+		else
+		{
+			i += 2;
+			while (archive[i] != 0)
+			{
+				(*filesize)++;
+				i++;
+			}
+			if (i % 2 == 0) i--;
+		}
+	}
+
+	*data = (unsigned char *)malloc((*filesize) * sizeof(unsigned char));
+
+	debug("Archive size: %ld\n",archivesize);
+	debug("File size: %ld\n",*filesize);
+
+	// decompress file
+	for (i = 0,j = 0; i < archivesize - 1; i += 2)
+	{
+		count = archive[i];
+		val = archive[i+1];
+		if (count == 0)
+		{
+			i += 2;
+			while (archive[i] != 0)
+			{
+				(*data)[j] = archive[i];
+				i++;
+				j++;
+			}
+			if (i % 2 == 0) i--;
+			continue;
+		}
+		while (count > 0)
+		{
+			(*data)[j] = val;
+			j++;
+			count--;
+		}
+	}
+
+	return;
 }
 
 void rle_a(unsigned char* data, unsigned char **archive, long int filesize, long int *archivesize)
@@ -113,7 +265,12 @@ void decompress(char* file)
 
 	// compress with RLE
 	printf("Decompressing file... %s\n", file);
-	rle_x(&data, archive, &filesize, archivesize);
+
+#ifdef MARKERS
+	rle_markers_x(&data, archive, &filesize, archivesize);
+#else
+	rle_markers_x(&data, archive, &filesize, archivesize);
+#endif
 
 	// append file with *.a file extension
 	strcpy(outfile,file);
@@ -143,7 +300,12 @@ void compress(char* file)
 
 	// compress with RLE
 	printf("Compressing file... %s\n", file);
+
+#ifdef MARKERS
+	rle_markers_a(data, &archive, filesize, &archivesize);
+#else
 	rle_a(data, &archive, filesize, &archivesize);
+#endif
 
 	// append file with *.a file extension
 	strcpy(outfile,file);
