90c348edd031b40cc141cfd2ad4c14735396a036
Griffin <gdube@g.clemson.edu>
2021-02-14 22:17:43 -0500

    Adding helper functions for LZW Compression

--- lab3.c
+++ lab3.c
@@ -16,12 +16,22 @@
 #define TRUE 1
 #define FALSE 0
 
+typedef struct table
+{
+	unsigned short int key;
+	unsigned char* val;
+	int sym_length;
+}table_t;
+
 void parse_input(int argc, char** argv, char* filename, char* compress, char* decompress);
 void read_file(char* filename, unsigned char** data, long int *filesize);
 void write_file(char* filename, unsigned char* data, long int filesize);
 void lzw_a(unsigned char* data, unsigned char **archive, long int filesize, long int *archivesize);
 void lzw_x(unsigned char** data, unsigned char* archive, long int *filesize, long int archivesize);
 void print_stats(char* infile, char* outfile, long int filesize, long int archivesize);
+void init_table(table_t** table, unsigned short int* tablesize);
+void print_table(table_t* table,unsigned short int from, unsigned short int to, unsigned short int tablesize);
+void destroy_table(table_t** table, unsigned short int tablesize);
 void compress(char* file);
 void decompress(char* file);
 
@@ -38,8 +48,101 @@ int main (int argc, char** argv)
 	return 0;
 }
 
+char find_val(table_t table, unsigned short int length, unsigned char* value, unsigned short int val_len, unsigned short int k)
+{
+	for (int i = 0; i < length; i++)
+	{
+		for (int j = 0; j < val_len; j++)
+		{
+		}
+	}
+	return 0;
+}
+
+void array_cat(unsigned char **A, unsigned char c, int *len)
+{
+	(*len)++;
+	*A = realloc(*A, *len * sizeof(unsigned char));
+	(*A)[*len - 1] = c;
+	return;
+}
+
+void init_table(table_t** table, unsigned short int* tablesize)
+{
+	*table = (table_t *)malloc(ENTRIES * sizeof(table_t));
+	for (unsigned short int i = 0; i < ENTRIES; i++)
+	{
+		(*table)[i].key = i;
+		(*table)[i].val = NULL;
+		(*table)[i].sym_length = 0;
+	}
+	for (int i = 0; i < 256; i++)
+	{
+		(*table)[i].val = (unsigned char *)malloc(sizeof(unsigned char));
+		(*table)[i].val[0] = i;
+		(*table)[i].sym_length++;
+		debug("Adding value %c for key %d\n",(*table)[i].val[0],(*table)[i].key);
+	}
+	*tablesize = 256;
+
+
+	return;
+}
+
+void destroy_table(table_t** table, unsigned short int tablesize)
+{
+	for (unsigned short int i = 0; i < tablesize; i++)
+	{
+		free((*table)[i].val);
+		(*table)[i].val = NULL;
+	}
+	free(*table);
+	table = NULL;
+	return;
+}
+
+void print_table(table_t* table,unsigned short int from, unsigned short int to, unsigned short int tablesize)
+{
+	assert(to <= tablesize && "TABLE IS NOT LARGE ENOUGH TO PRINT REQUESTED VALUES");
+	for (int i = from; i < to; i++)
+	{
+		debug("Key: %d \tVal: ",table[i].key);
+		for (int j = 0; j < table[i].sym_length; j++)
+		{
+			debug("%c ",table[i].val[j]);
+		}
+		debug("%c",'\n');
+	}
+	return;
+}
+
 void lzw_a(unsigned char* data, unsigned char **archive, long int filesize, long int *archivesize)
 {
+	table_t* table = NULL;
+	unsigned short int tablesize = 0;
+	unsigned short int keyval;
+	unsigned char *p, c;
+
+	*archive = (unsigned char *)malloc(filesize * 2 * sizeof(unsigned char));
+
+	init_table(&table, &tablesize);
+	print_table(table, 255, 300, tablesize);
+
+	p = data[0];
+	for (int i = 0; i < filesize; i++)
+	{
+		c = data[i + 1];
+		if (find_val(table,&keyval))
+		{
+		}
+		else
+		{
+		}
+
+	}
+
+	destroy_table(&table, tablesize);
+
 
 	return;
 }
@@ -99,12 +202,12 @@ void compress(char* file)
 	strcpy(outfile,file);
 	strcat(outfile,".a");
 
-	write_file(outfile, archive, archivesize);
+//	write_file(outfile, archive, archivesize);
 
 	free(data);
 	free(archive);
 
-	print_stats(file, outfile, filesize, archivesize);
+//	print_stats(file, outfile, filesize, archivesize);
 
 	return;
 }
